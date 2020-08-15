fc0c76e6d337ad7b94f0d8e4db9ba6cdb73e3541
Akshay Jindal <intendefer@gmail>
2020-08-14 20:09:52 -0400

    Level 1

--- .ipynb_checkpoints/AssociateProfitSplitter-checkpoint.sol
+++ .ipynb_checkpoints/AssociateProfitSplitter-checkpoint.sol
@@ -1,76 +1,43 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "pragma solidity ^0.5.0;\n",
-    "\n",
-    "// lvl 1: equal split\n",
-    "contract AssociateProfitSplitter {\n",
-    "    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.\n",
-    "    address public payable employee_one;\n",
-    "    address public payable employee_two;\n",
-    "    address public payable employee_three;\n",
-    "\n",
-    "    constructor(address payable _one, address payable _two, address payable _three) public {\n",
-    "        employee_one = _one;\n",
-    "        employee_two = _two;\n",
-    "        employee_three = _three;\n",
-    "    }\n",
-    "\n",
-    "    function balance() public view returns(uint) {\n",
-    "        return address(this).balance;\n",
-    "    }\n",
-    "\n",
-    "    function deposit() public payable {\n",
-    "        // @TODO: Split `msg.value` into three\n",
-    "        uint amount = msg.value/3; // Your code here!\n",
-    "\n",
-    "        // @TODO: Transfer the amount to each employee\n",
-    "        employee_one.transfer(amount);\n",
-    "        employee_two.transfer(amount);\n",
-    "        employee_three.transfer(amount);\n",
-    "        \n",
-    "\n",
-    "\n",
-    "        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)\n",
-    "        //uint256 remainder = msg.value % 50000000000000000;\n",
-    "        //msg.sender.send(remainder);\n",
-    "        uint256 remainder = msg.value % 3;\n",
-    "        msg.sender.transfer(remainder);\n",
-    "    }\n",
-    "\n",
-    "    function() external payable {\n",
-    "        // @TODO: Enforce that the `deposit` function is called in the fallback function!\n",
-    "        \n",
-    "        deposit();\n",
-    "        }\n",
-    "    }\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 4
-}
+wpragma solidity ^0.5.0;
+
+// lvl 1: equal split
+contract AssociateProfitSplitter {
+    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    address public payable employee_one;
+    address public payable employee_two;
+    address public payable employee_three;
+
+    constructor(address payable _one, address payable _two, address payable _three) public {
+        employee_one = _one;
+        employee_two = _two;
+        employee_three = _three;
+    }
+
+    function balance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function deposit() public payable {
+        // @TODO: Split `msg.value` into three
+        uint amount = msg.value/3; // Your code here!
+
+        // @TODO: Transfer the amount to each employee
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
+        
+
+
+        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
+        //uint256 remainder = msg.value % 50000000000000000;
+        //msg.sender.send(remainder);
+        uint256 remainder = msg.value % 3;
+        msg.sender.transfer(remainder);
+    }
+
+    function() external payable {
+        // @TODO: Enforce that the `deposit` function is called in the fallback function!
+        
+        deposit();
+        }
+    }
